4b9d4dc3b7bb43beff42f6def1dba5a8bcd42600
WuYoTing <hakosaki314@gmail.com>
2021-04-05 15:37:12 +0800

    feat: add fake user agent middlewares

--- Isthereanyclothes_scrapy/Isthereanyclothes_scrapy/spiders/gu_tw.py
+++ Isthereanyclothes_scrapy/Isthereanyclothes_scrapy/spiders/gu_tw.py
@@ -1,17 +1,19 @@
 import scrapy
-import bs4
-import math
 
 
 class GuTwSpider(scrapy.Spider):
     name = 'gu-tw'
-    allowed_domains = ['www.gu-global.com/tw']
-    start_urls = ['https://www.gu-global.com/tw/store/search?qbrand=20&qclv1=001']
+
+    def start_requests(self):
+        urls = [
+            'https://www.gu-global.com/tw/store/search?qbrand=20&qclv1=001'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        man_index_bs = bs4.BeautifulSoup(response.text, "lxml")
-        man_prod_amount = int(
-            man_index_bs.select('#content .blkPaginationTop tr th')[0].text.replace(
-                '搜尋結果：', '').replace('件', ''))
-        man_prod_page = math.ceil(man_prod_amount / 48)
-        print(man_prod_amount + ' : ' + man_prod_page)
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
