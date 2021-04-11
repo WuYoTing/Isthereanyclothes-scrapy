9e4b30f3cc0c7a3af6b9d42101f9a87fc24e9b17
WuYoTing <hakosaki314@gmail.com>
2021-04-11 23:45:58 +0800

    feat: 新增爬取衣服資料

--- Isthereanyclothes_scrapy/spiders/gu_tw.py
+++ Isthereanyclothes_scrapy/spiders/gu_tw.py
@@ -1,9 +1,7 @@
-import json
+import random
 import math
 import scrapy
 from bs4 import BeautifulSoup
-from scrapy.spidermiddlewares.httperror import HttpError
-from twisted.internet.error import DNSLookupError, TCPTimedOutError
 
 import Isthereanyclothes_scrapy.items as items
 
@@ -13,7 +11,7 @@ class GuTwSpider(scrapy.Spider):
 
     # set custom settings
     custom_settings = {
-        'DOWNLOAD_DELAY': 0.6283,
+        'DOWNLOAD_DELAY': random.uniform(0.7, 1),
         'DEFAULT_REQUEST_HEADERS': {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
@@ -26,21 +24,21 @@ class GuTwSpider(scrapy.Spider):
         self.man_size = 0
         self.woman_size = 0
         self.gu_base_url = 'https://www.gu-global.com/tw/store/'
-        self.gu_api_extend_url = 'ApiGetProductInfo.do?format=json&product='
+        self.gu_api_extend_url = 'ApiGetProductInfo.do?product='
 
     def start_requests(self):
         man_index_url = self.gu_base_url + 'search?qbrand=20&qclv1=' + self.man
         yield scrapy.Request(
             url=man_index_url,
             priority=1,
-            cb_kwargs=dict(sex='man'),
+            cb_kwargs=dict(sex=1),
             callback=self.parse_page_size
         )
         woman_index_url = self.gu_base_url + 'search?qbrand=20&qclv1=' + self.woman
         yield scrapy.Request(
             url=woman_index_url,
             priority=2,
-            cb_kwargs=dict(sex='woman'),
+            cb_kwargs=dict(sex=2),
             callback=self.parse_page_size
         )
 
@@ -51,11 +49,11 @@ class GuTwSpider(scrapy.Spider):
         )[0].text.replace('搜尋結果：', '').replace('件', ''))
         man_prod_page = math.ceil(man_prod_amount / 48)
 
-        if sex == 'man':
+        if sex == 1:
             sex_code = self.man
             print('男 共 ' + str(man_prod_page) + ' 頁 ' + str(man_prod_amount) + " 個產品" + '  ' + str(
                 response.status))
-        elif sex == 'woman':
+        elif sex == 2:
             sex_code = self.woman
             print('女 共 ' + str(man_prod_page) + ' 頁 ' + str(man_prod_amount) + " 個產品" + '  ' + str(
                 response.status))
@@ -67,20 +65,20 @@ class GuTwSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=parse_page_url,
                 priority=3,
-                cb_kwargs=dict(sex=sex, page_index=page_index),
+                cb_kwargs=dict(sex=sex),
                 callback=self.get_prod_url
             )
 
-    def get_prod_url(self, response, sex, page_index):
+    def get_prod_url(self, response, sex):
         man_page_bs = BeautifulSoup(response.text, "lxml")
         for category in man_page_bs.select('#blkMainItemList .unit .info .name a'):
             product_url = category['href']
             if self.gu_base_url + 'goods/' in product_url:
                 product_num = product_url.split(self.gu_base_url + 'goods/')[1]
                 product_api_url = self.gu_base_url + self.gu_api_extend_url + product_num
-                if sex == 'man':
+                if sex == 1:
                     self.man_size += 1
-                    print('man ' + str(page_index) + ' ' + str(product_api_url) + ' ' + str(
+                    print('man ' + str(product_api_url) + ' ' + str(
                         response.status) + ' ' + str(self.man_size))
                     yield scrapy.Request(
                         url=product_api_url,
@@ -88,9 +86,9 @@ class GuTwSpider(scrapy.Spider):
                         cb_kwargs=dict(sex=sex),
                         callback=self.get_product_info
                     )
-                elif sex == 'woman':
+                elif sex == 2:
                     self.woman_size += 1
-                    print('woman ' + str(page_index) + ' ' + str(product_api_url) + ' ' + str(
+                    print('woman ' + str(product_api_url) + ' ' + str(
                         response.status) + ' ' + str(self.woman_size))
                     yield scrapy.Request(
                         url=product_api_url,
@@ -100,7 +98,23 @@ class GuTwSpider(scrapy.Spider):
                     )
 
     def get_product_info(self, response, sex):
-        jsonresponse = json.loads(response.text)
-        if jsonresponse['GoodsInfo']:
+        json_resp = response.json()
+        if json_resp['GoodsInfo']:
             # 初始化item
-            print(jsonresponse['GoodsInfo']['goods']['goodsNm'])
+            clothes_item = items.ClothesItem()
+            clothes_images_item = items.ClothesImagesItem()
+            clothes_sale_tag_item = items.ClothesSaleTagItem()
+            clothes_price_log_item = items.ClothesPriceLogItem()
+            # collect clothes data
+            clothes_item['name'] = json_resp['GoodsInfo']['goods']['goodsNm']
+            clothes_item['sex'] = sex
+            clothes_item['brand_id'] = 1
+            clothes_item['prod_number'] = json_resp['GoodsInfo']['goods']['l1GoodsCd']
+            clothes_item['description'] = json_resp['GoodsInfo']['goods']['dtlExp']
+            clothes_item['material'] = json_resp['GoodsInfo']['goods']['materialInfo']
+            clothes_item['url'] = self.gu_base_url + 'goods/' + json_resp['GoodsInfo']['goods'][
+                'l1GoodsCd']
+            clothes_item['size_url'] = self.gu_base_url + "support/size/" + \
+                                       json_resp['GoodsInfo']['goods']['l1GoodsCd'] + "_size.html"
+            print(clothes_item)
+
